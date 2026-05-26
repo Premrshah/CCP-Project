51f2e297e4466c4410ae4f0ffdceffb2b4071f53
Prem Shah <mrpremrshah@gmail.com>
2026-05-26 09:26:01 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+def load_data(dataset_name):
+    if dataset_name == "Synthetic":
+        return pd.read_csv("CCP/synthetic.csv")
+    elif dataset_name == "Organic":
+        return pd.read_csv("CCP/organic.csv") 
+
+st.title("Data Visualization for Fast Fashion!")
+
+dataset_option = st.selectbox(
+    "Choose a dataset to view:",
+    ("Synthetic", "Organic")
+)
+
+df = load_data(dataset_option)
+
+st.line_chart(df, x="x_value", y="y_value")
+st.dataframe(df)
+
+
